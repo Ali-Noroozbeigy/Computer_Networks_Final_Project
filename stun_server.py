96d15f0326b5198a317b0e0d1c23b48bd90b4008
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-05-27 23:52:46 +0430

    add general post method

--- stun_server.py
+++ stun_server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,6 +11,16 @@ class RequestHandler(BaseHTTPRequestHandler):
 
             self.wfile.write(bytes(message, 'utf8'))
         return
+    
+    def do_POST(self):
+        if self.path == '/add-data':
+            content_length = int(self.headers.get('Content-length', 0))
+
+            post_data = self.rfile.read(content_length)
+            dec = post_data.decode('utf-8')
+            parsed = json.loads(dec)
+            print(parsed['message'])
+
 
 
 server_address = ('', 8000)
