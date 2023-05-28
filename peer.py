28202989e72bf22814b0e65b2baf5ab9e3d665c5
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-05-28 23:17:53 +0430

    implement first listening

--- peer.py
+++ peer.py
@@ -1,7 +1,8 @@
 import requests
 
 
-TCP_PORT = 2896
+UDP_PORT = 2896
+HOST = '127.0.0.1'
 
 
 def introduce():
@@ -28,6 +29,28 @@ def get_user_ip(username):
     
     info = response.json() 
     print(info['IP'], info['PORT'])
+
+
+def wait_for_peers_to_call():
+    import socket
+
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udp_socket.bind((HOST, UDP_PORT))
+
+    print('wait for peers to call...')
+    
+    while True:
+        data, addr = udp_socket.recvfrom(1024)
+        agree = input(f'New Connection From {addr}, Agree? [Y/n]')
+
+        if agree == 'Y':
+            pass
+        else:
+            continue
+
+
+
 
 introduce()
 get_list_users()
